f52af6c5aa47cb63293a8bbad239ad5097139505
dePaula <93327141+dePaulaRodrigo@users.noreply.github.com>
2022-06-20 08:50:17 -0300

    Update Roll15_Challenge.py

--- Roll15_Challenge.py
+++ Roll15_Challenge.py
@@ -14,28 +14,47 @@ def roll15():
     while b1_3 > 3:
         b1_3 = roll6()
 
-    # Get random numbers from 4 to 9
-    b4_9 = roll6() + 3
+    # Get random numbers from 4 to 6
+    b4_6 = roll6()
+    while b4_6 < 4:
+        b4_6 = roll6()
 
-    # Get random numbers from 10 to 15
-    b10_15 = roll6() + 9
+    # Get random numbers from 7 to 9
+    b7_9 = roll6() + 3
+    while b7_9 < 7:
+        b7_9 = roll6() + 3
+
+    # Get random numbers from 10 to 12
+    b10_12 = roll6() + 9
+    while b10_12 > 12:
+        b10_12 = roll6() + 9
+
+    # Get random numbers from 13 to 15
+    b13_15 = roll6() + 9
+    while b13_15 < 13:
+        b13_15 = roll6() + 9
 
     # List with the three variables
-    c = [b1_3, b4_9, b10_15]
+    c = [b1_3, b4_6, b7_9, b10_12, b13_15]
 
-    # Get a random number from 1 to 3
+    # Get a random number from 1 to 5
     d = roll6()
-    while d > 3:
+    while d > 5:
         d = roll6()
 
-    # Choose one element from the list and print it based on variable d
+    # Pick one element from the list and print it based on variable d
     if d == 1:
-        print(c[0])
+        e = c[0]
     elif d == 2:
-        print(c[1])
+        e = c[1]
     elif d == 3:
-        print(c[2])
+        e = c[2]
+    elif d == 4:
+        e = c[3]
+    elif d == 5:
+        e = c[4]
 
+    return e
 
 # Call the function
- roll15()
+print(roll15())
